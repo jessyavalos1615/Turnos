8e0622ba8706b0fa0d3268689b952bcc19bd067d
Jessy Avalos Barrales <31834551+jessyavalos1615@users.noreply.github.com>
2018-09-14 10:17:31 -0500

    Commit 7.2
    Documentacion al corriente y enlaze los coomponentes xml con sus variables

--- app/src/main/java/com/example/uli_b/turnos/MainActivity.java
+++ app/src/main/java/com/example/uli_b/turnos/MainActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,13 +39,23 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Variables para comunicar los elementos en el xml
     TextView conection;
+    ImageButton btn_mail, btn_service,btn_powers,
+                btn_sends, btn_copy, btn_acord,
+                btn_travel, btn_scan, btn_tax,
+                btn_car, btn_forms, btn_money_ord,
+                btn_notary, btn_phone, btn_pack,
+                btn_photo, btn_loan, btn_temp_car,
+                btn_traductor;
 
+
+    //Variables necesarias para mandar a llamar las herramientas del BLUETOOTH
     BluetoothAdapter mBluetoothAdapter;
     BluetoothSocket mmSocket;
     BluetoothDevice mmDevice;
 
-    // needed for communication to bluetooth device / network
+    //Variables necesarias para establecer la comunicación entre dispositivos.
     OutputStream mmOutputStream;
     InputStream mmInputStream;
     Thread workerThread;
@@ -58,15 +69,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            conection = (TextView)findViewById(R.id.conexion);
+        //Enlazamos los elementos del xml con sus correspondientes variables.
+        conection = (TextView)findViewById(R.id.conexion);
+        btn_acord = (ImageButton)findViewById(R.id.acord);
+        btn_car = (ImageButton)findViewById(R.id.car);
+        btn_copy = (ImageButton)findViewById(R.id.copy);
+        btn_forms = (ImageButton)findViewById(R.id.forms);
+        btn_loan = (ImageButton)findViewById(R.id.loan);
+        btn_mail = (ImageButton)findViewById(R.id.mail);
+        btn_money_ord = (ImageButton)findViewById(R.id.moneyOrd);
+        btn_notary = (ImageButton)findViewById(R.id.notary);
+        btn_pack = (ImageButton)findViewById(R.id.pack);
+        btn_phone = (ImageButton)findViewById(R.id.phone);
+        btn_photo = (ImageButton)findViewById(R.id.photos);
+        btn_powers = (ImageButton)findViewById(R.id.powers);
+        btn_scan = (ImageButton)findViewById(R.id.scan);
+        btn_sends = (ImageButton)findViewById(R.id.sends);
+        btn_service = (ImageButton)findViewById(R.id.service);
+        btn_tax = (ImageButton)findViewById(R.id.tax);
+        btn_temp_car = (ImageButton)findViewById(R.id.temp_car);
+        btn_traductor = (ImageButton)findViewById(R.id.traduction);
+        btn_travel = (ImageButton)findViewById(R.id.travel);
 
+        //Mandamos a llamar los metodos findBT() y openBT() para buscar un dispositivo BT y abrir la conexion.
+        try {
             findBT();
             openBT();
         }catch(IOException e) {
             e.printStackTrace();
         }
     }
+
     //Seccion de codigo para conexion web services
     private class InvocarServicioRegistrarAlumnos extends AsyncTask<Void, Integer, Void> {
 
@@ -138,6 +171,7 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
     private String getPostDataString(HashMap<String, String> params) throws UnsupportedEncodingException {
         StringBuilder result = new StringBuilder();
         boolean first = true;
@@ -150,6 +184,7 @@ public class MainActivity extends AppCompatActivity {
         }
         return result.toString();
     }
+
     private void jsonInsertLocal(Void result)  {
 
 
@@ -172,27 +207,37 @@ public class MainActivity extends AppCompatActivity {
     }
     //Terminacion de codigo del web services
 
+    //Metodo para encontrar la conexion BT con la impresora.
     void findBT() {
 
         try {
+
+            //Aqui instanciamos el bluetooth adapter para tener accesso a los metodos.
             mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
+            //En este if comprobamos que el dispositivo a utilizar, disponga de un modulo bluetooth para un buen funcionamiento.
             if(mBluetoothAdapter == null) {
                 conection.setText("No bluetooth adapter available");
             }
 
+            //Aqui comprobamos que se encuentre activo el bluetooth.
+            // En caso de que no esté activo se le mostrara un cuadro de dialogo al usuario con la peticion para activarlo.
             if(!mBluetoothAdapter.isEnabled()) {
                 Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                 startActivityForResult(enableBluetooth, 0);
             }
 
+            //Aqui instanciamos una variable para la manipulacion de dispositivos.
             Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
 
+            //Aqui hacemos la comparacion para saber si existen dispositivos emparejados con bluetooth en nuestro dispositivo.
             if(pairedDevices.size() > 0) {
+
+                //Aqui realizamos una busqueda en el historial de dispositovos emparejados
                 for (BluetoothDevice device : pairedDevices) {
 
-                    // RPP300 is the name of the bluetooth printer device
-                    // we got this name from the list of paired devices
+                    //Durante la busqueda si se encuentra el nombre de la impresora que deseamos conectar
+                    //Se almaenara en la variable mmDevice
                     if (device.getName().equals("RPP300")) {
                         mmDevice = device;
                         break;
@@ -200,6 +245,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
 
+            //Aqui se mandara en el TextView para informar que se encontro la conexion de la impresora
             conection.setText("Bluetooth device found.");
 
         }catch(Exception e){
@@ -207,19 +253,25 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
+    //Metodo para abrir la conexion BT.
     void openBT() throws IOException {
         try {
 
-            // Standard SerialPortService ID
+            // Se establece el serial del puerto de sercivio estandar paa la ceonexion.
             UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805f9b34fb");
+
+            //Se establece el Socket para la conexion y se conecta
             mmSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
             mmSocket.connect();
+
+            //Declaramos las variables para envia y recivir datos durante la conexion
             mmOutputStream = mmSocket.getOutputStream();
             mmInputStream = mmSocket.getInputStream();
 
+            //Llamamos el metodo que estara a la espera de datos
             beginListenForData();
 
+            //Mandamos al TextView par informar que la conexion se  ah abierto.
             conection.setText("Bluetooth Opened");
 
         } catch (Exception e) {
@@ -227,6 +279,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Metodo utilizado para recivir datos desde otro dispositivo
     void beginListenForData() {
         try {
             final Handler handler = new Handler();
@@ -296,6 +349,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Metodo para enviar los datos a la impresora
     public void sendData() throws IOException {
         try {
 
@@ -313,6 +367,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //En caso de que la aplicacion se cierre por completo, se manda a llamar el metodo closeBT().
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -323,6 +378,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Metodo que sirve para cerar todos los servicios necesarios para la conexion.
     void closeBT() throws IOException {
         try {
             stopWorker = true;
@@ -335,6 +391,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void sendData(View view) {
-    }
 }
